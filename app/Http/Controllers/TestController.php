c3092db381b9db37ec89633b76e0f561007807b1
Nguyen Phuong Tan <tannguyen.10102004@gmail.com>
2024-05-04 16:06:27 +0700

    .

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -12,23 +12,19 @@ class TestController extends Controller
         $client = new Client([
             'verify' => false,
         ]);
-
         $response = $client->request('GET', 
         'https://api.topdev.vn/td/v2/jobs?ordering=newest_job&page_size=32&packages=basic-plus&locale=vi_VN&fields[job]=id,company,title,skills_ids,salary,addresses,published,detail_url,slug');
         $data = json_decode($response->getBody()->getContents(), true);
+        $filteredData = array_map(function ($job) {
+            return [
+                'id' => $job['id'],
+                'title' => $job['title'],
+                'content' => $job['content'],
+                'company_id' => $job['company']['id'],
+            ];
+        }, $data['data']);
 
-        // // Xử lý dữ liệu tại đây...
-        // $filteredData = array_map(function ($job) {
-        //     return [
-        //         'id' => $job['id'],
-        //         'title' => $job['title'],
-        //         'content' => $job['content'],
-        //         'company_id' => $job['company']['id'],
-        //     ];
-        // }, $data['data']);
-
-        // $data['data'] = $filteredData;
-
+        $data['data'] = $filteredData;
         return response()->json($data);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -10,6 +10,7 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
+
 Route::get('company/job', [CompanyApiController::class, "getJob"]);
 Route::get('company/{id}', [CompanyApiController::class, "show"]);
 Route::get('/test', [TestController::class, 'handleAPI']);
