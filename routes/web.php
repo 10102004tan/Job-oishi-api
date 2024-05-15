25b4fde60a0ddd5b66f1248bba689e8b2ff81458
Nguyen Phuong Tan <tannguyen.10102004@gmail.com>
2024-05-15 17:22:40 +0700

    route web.php update

--- routes/web.php
+++ routes/web.php
@@ -6,8 +6,10 @@ use App\Http\Controllers\CompanyController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\JobController;
+use App\Http\Controllers\TestController;
 
 Route::get('/',[ HomeController::class,'index'])->name('home');
 Route::resource('jobs', JobController::class);
 Route::resource('companies', CompanyController::class);
 Route::resource('benefits', BenefitController::class);
+
