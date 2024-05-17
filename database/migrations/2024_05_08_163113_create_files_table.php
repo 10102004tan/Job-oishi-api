74ddc882e585eab47237f23795a78c7ea266ef56
Datto098 <dat61222@gmail.com>
2024-05-17 10:45:42 +0700

    update branch

--- database/migrations/2024_05_08_163113_create_files_table.php
+++ database/migrations/2024_05_08_163113_create_files_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('files', function (Blueprint $table) {
             $table->id();
             $table->string("name");
-            $table->id("user_id");
+            $table->integer("user_id");
             $table->timestamps();
         });
     }
