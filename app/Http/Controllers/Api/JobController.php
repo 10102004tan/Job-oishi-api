aa58d22a10fd214fb92edb7984f2b309ec02bb5c
Nguyen Phuong Tan <tannguyen.10102004@gmail.com>
2024-06-06 00:32:21 +0700

    Refactor JobController.php

--- app/Http/Controllers/Api/JobController.php
+++ app/Http/Controllers/Api/JobController.php
@@ -13,7 +13,6 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Http;
-
 class JobController extends Controller
 {
 
